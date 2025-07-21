157bbb30ce2de8360ac7687cfb8ecee81d03d3e7
Ahmed mosaad <133489264+tosnami@users.noreply.github.com>
2025-07-21 19:12:26 +0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,23 @@
+
+let num1 = 10;
+let num2 = 20;
+
+console.log("Is num1 less than num2?", num1 < num2);
+console.log("Is num1 greater than num2?", num1 > num2);
+console.log("Is num1 equal to num2?", num1 == num2);
+
+let sum = num1 + num2;
+console.log("Sum =", sum);
+
+let subtract = num2 - num1;
+console.log("Subtraction =", subtract);
+
+let multiply = num1 * num2;
+console.log("Multiplication =", multiply);
+
+let divide = num2 / num1;
+console.log("Division =", divide);
+
+let num3 = 30;
+let average = (num1 + num2 + num3) / 3;
+console.log("Average =", average);
